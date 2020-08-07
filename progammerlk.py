7fc818492f1ce52ea5dc1f32f4f6dc5e2b98a694
Security Vx <68503915+BangArex@users.noreply.github.com>
2020-08-07 09:36:57 +0700

    Add files via upload

---
+++ progammerlk.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python2
+# coding=utf-8
+
+import os,sys,time,datetime,json,urllib,re,hashlib,cookielib
+import random
+
+os.system('clear')
+time.sleep(0.46)
+
+print """\033[31;1m█████████           \033[33;1m╔════•ೋೋ•════╗
+\033[31;1m█▄█████▄█                \033[0;32mNumeV
+\033[31;1m█▼▼▼▼▼		    ╚════•\033[37;1mೋೋ\033[31;1m•════╝
+\033[31;1m█ \033[0;30mProgammer
+\033[37;1m█▲▲▲▲▲
+█████████
+ ██ ██"""
+
+time.sleep(1)
+print ""
+print ""
+time.sleep(1)
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+time.sleep(0.1)
+mengetik('       \033[33;1m[\033[31;1m» \033[0;36mTools \033[31;1m«\033[33;1m]')
+print ""
+time.sleep(0.46)
+
+print ""
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+time.sleep(0.1)
+mengetik(' \033[33;1m[ \033[37;1m1\033[31;1m.\033[33;1m]  \033[37;1mSantet Online')
+print ""
+time.sleep(0.46)
+print ""
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+time.sleep(0.1)
+mengetik(' \033[33;1m[ \033[37;1m2\033[31;1m.\033[33;1m]  \033[37;1mHack Satelit')
+print ""
+time.sleep(0.46)
+
+print ""
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+time.sleep(0.1)
+mengetik(' \033[33;1m[ \033[37;1m3\033[31;1m.\033[33;1m]  \033[37;1mHack Mantan :V')
+print ""
+time.sleep(0.46)
+
+print ""
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+time.sleep(0.1)
+mengetik(' \033[33;1m[ \033[37;1m4\033[31;1m.\033[33;1m]  \033[37;1mSadap Wa [Palsu Goblok :v]')
+print ""
+time.sleep(0.46)
+
+print ""
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+time.sleep(0.1)
+mengetik(' \033[33;1m[ \033[37;1m5\033[31;1m.\033[33;1m]  \033[37;1mKeluar/Exit')
+print ""
+time.sleep(0.46)
+
+print ""
+pilih = raw_input(' \033[31;1m[\033[34;1m#\033[0;32mPilih Asw! \033[36;1m: ')
+
+
+if pilih == "1":
+
+   time.sleep(1)
+   os.system('clear')
+   print " \033[32;1mCoba Lu Ke \033[31;1mDukun \033[32;1mSana Pas Korona :v, Gw Males Bikin Ni Sc Anjink :V "
+   time.sleep(5)
+   os.system('python2 progammerlk.py')
+
+if pilih == "2":
+
+   time.sleep(1)
+   os.system('clear')
+   print " \033[32;1mLu Ngapa Dah Njir Nyari Kek Gini Oakaowk, Mending Lu \033[31;1mPergi \034[32;1mAja Ke \033[31;1mSatelit \033[32;1mSono Anjg , kwkwkwk mabal blo -!-"
+   time.sleep(5)
+   os.system('python2 progammerlk.py')
+
+if pilih == "3":
+
+   time.sleep(1)
+   os.system('clear')
+   print " Lu Cari Aja Njir Mantannya Ke Mall Atau Kemana Mana Wkkwkwkw, Bye Lah Selesai Sudha -!-"
+   time.sleep(5)
+   os.system('python2 progammerlk.py')
+
+if pilih == "4":
+   os.system('clear')
+   print " Males Ngetik Ama Warnain Gw Asuu aowkaow"
+   time.sleep(1)
+   os.system('python2 progammerlk.py')
+
+if pilih == "5":
+   os.system('clear')
+   print " Ketik CTRL + Z "
+   time.sleep(100)
+
